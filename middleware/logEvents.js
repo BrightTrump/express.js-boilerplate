9b2974b4f9e2afa4176ea3822eeeb750cf38f77b
Bright Munemu <brightmunemu@gmail.com>
2025-10-03 01:28:40 +0100

    add files

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -23,10 +23,46 @@ const logEvents = async (message, logName) => {
   }
 };
 
+// const logger = (req, res, next) => {
+//   const origin = req.get("Origin") || "no-origin";
+//   const referer = req.get("Referer") || "no-referer";
+//   const ua = req.get("User-Agent");
+
+//   logEvents(`${req.method}\t${req.header.origin}\t${req.url}`, "reqLog.txt");
+//   //   console.log(`${req.method} ${req.path}`);
+//   console.log(
+//     `${new Date().toISOString()}  ${
+//       req.method
+//     }  origin=${origin}  referer=${referer}  ua=${ua}  url=${req.url}`
+//   );
+
+//   next();
+// };
+
+// middleware/logEvents.js
+
 const logger = (req, res, next) => {
-  logEvents(`${req.method}\t${req.header.origin}\t${req.url}`, "reqLog.txt");
-  console.log(`${req.method} ${req.path}`);
+  const origin = req.get("Origin") || "[no Origin header]";
+  const referer = req.get("Referer") || "[no Referer header]";
+  const ua = req.get("User-Agent") || "[no User-Agent]";
+
+  const logItem = `${new Date().toISOString()}\t${
+    req.method
+  }\torigin=${origin}\treferer=${referer}\turl=${req.url}\tua=${ua}\n`;
+
+  console.log(logItem);
+
+  fs.appendFile(
+    path.join(__dirname, "..", "logs", "reqLog.txt"),
+    logItem,
+    (err) => {
+      if (err) throw err;
+    }
+  );
+
   next();
 };
 
-module.exports = {logger, logEvents};
+module.exports = { logger };
+
+module.exports = { logger, logEvents };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 const cors = require("cors");
 const { logger } = require("./middleware/logEvents");
 const PORT = process.env.PORT || 3500;
