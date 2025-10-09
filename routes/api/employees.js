4ac4ce8d2c3155fa403d6f3710cbc325742bac0c
Bright Munemu <brightmunemu@gmail.com>
2025-10-09 21:23:21 +0100

    update employee route

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const employeeController = require("../../controllers/employee-controller");
+const ROLES_LIST = require("../../config/roles_list");
+const verifyRoles = require("../../middleware/verify-roles");
 const verifyJWT = require("../../middleware/verify-jwt");
 
 router
   .route("/")
   .get(verifyJWT, employeeController.getAllEmployees)
-  .post(employeeController.createNewEmployee)
-  .patch(employeeController.updateEmployee)
-  .put(employeeController.updateEmployees)
-  .delete(employeeController.deleteEmployee);
+  .post(
+    verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
+    employeeController.createNewEmployee
+  )
+  .patch(
+    verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
+    employeeController.updateEmployee
+  )
+  .put(
+    verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
+    employeeController.updateEmployees
+  )
+  .delete(verifyRoles(ROLES_LIST.Admin), employeeController.deleteEmployee);
 
 router.route("/:id").get(employeeController.getEmployee);
 
